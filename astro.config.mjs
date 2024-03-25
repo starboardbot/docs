7aaeaaefe68d8e71f02e65cbad0740781f277876
ghost <ghostlylake@gmail.com>
2024-03-26 11:36:44 +1300

    test pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://starboardbot.github.io',
+	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'Starboard',
