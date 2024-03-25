f9ee18d54c7531e424dfc2a0c7aa3602b58c5284
ghost <ghostlylake@gmail.com>
2024-03-26 09:14:19 +1300

    content updates, bool & int types, twemoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,51 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Starboard',
+			favicon: 'favicon.png',
 			social: {
 				discord: 'https://discord.gg/rZgxfbH',
-				github: 'https://github.com/starboardbot/guide',
+				github: 'https://github.com/starboardbot/docs',
+				patreon: 'https://patreon.com/TheNoob27',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					autogenerate: {
-						directory: '/guides/'
-					}
+						directory: '/guides/',
+					},
 				},
+				{
+					label: 'Links',
+					items: [
+						{
+							label: 'Support Server',
+							link: 'https://discord.gg/rZgxfbH',
+						},
+						{
+							label: 'Vote for Starboard',
+							link: 'https://top.gg/bot/655390915325591629/vote',
+						},
+						{
+							label: 'Starboard Premium',
+							link: 'https://patreon.com/TheNoob27',
+						},
+					],
+				}
 			],
 			customCss: ['./src/tailwind.css'],
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						src: 'https://unpkg.com/twemoji@latest/dist/twemoji.min.js',
+						crossorigin: 'anonymous',
+					},
+				},
+				{
+					tag: 'script',
+					content: 'window.addEventListener(\'load\', () => {console.log(\'loaded\'); twemoji.parse(document.body)})'
+				},
+			],
 		}),
 		tailwind({ applyBaseStyles: false }),
 	],
--- src/content/docs/guides/getting-started.md
+++
@@ -1,7 +0,0 @@
----
-title: Getting started
-description: Get Starboard set up in your server
-sidebar:
-    order: 0
----
-
